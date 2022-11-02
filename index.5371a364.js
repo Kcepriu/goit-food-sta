9b4c61f8aa886fa2fd19d75d718d73774dca5777
Kcepriu <66989313+Kcepriu@users.noreply.github.com>
2022-11-02 20:59:19 +0000

    Deploying to gh-pages from @ Kcepriu/goit-food-sta@db25394abb40c7c8d6644c528c36cfa9bc9bb4a0 🚀

--- index.5371a364.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:800,spaceBetween:25,pagination:{el:".swiper-pagination"},navigation:{nextEl:".btn-slide__left",prevEl:".btn-slide__right"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.5371a364.js.map
---
+++ index.5681d3d6.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:800,spaceBetween:25,pagination:{el:".swiper-pagination"},navigation:{prevEl:".btn-slide__left",nextEl:".btn-slide__right"},scrollbar:{el:".swiper-scrollbar"},keyboard:{enabled:!0,onlyInViewport:!1}});
+//# sourceMappingURL=index.5681d3d6.js.map
