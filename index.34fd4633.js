ab11ea9524bf0c32b5e211c1eef2ddc037e5c5e7
Kcepriu <66989313+Kcepriu@users.noreply.github.com>
2022-11-04 13:28:47 +0000

    Deploying to gh-pages from @ Kcepriu/goit-food-sta@f1f7f93a9deb2f5e1f37d56d148fbf64ecc731ad 🚀

--- index.34fd4633.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),bodyScrollLock:document.querySelector("body")};document.querySelector("body");function e(){o.modal.classList.toggle("is-hidden"),o.bodyScrollLock.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.34fd4633.js.map
